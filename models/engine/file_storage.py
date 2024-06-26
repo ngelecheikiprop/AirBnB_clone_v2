6edb7575693c1b97e009292ec335217874221abb
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-26 17:01:51 +0300

    number 5, added documentation on the delete function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -54,8 +54,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
     def delete(self, obj=None):
+        """
+        Deletes an object from storage"""
         if obj == None:
             return;
         key = obj.to_dict()['__class__'] + '.' + obj.id 
