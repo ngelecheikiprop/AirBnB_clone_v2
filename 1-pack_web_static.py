e09af013b0cf1e779b2f9c08ca1507ecff06989e
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-11 09:15:03 +0300

    done and tested

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 'archieve web_static folder to deploy'
 from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
     'compress the webstatic folder into versions folder'
-    local('mkdir -p versions', shell=True)
-    local('time_now=$(date "+%Y%m%d%H%M%S")', shell=True)
-    local('file_name=web_static_$time_now.tgz', shell=True)
-    local('mypath=versions/$file_name', shell=True)
-    local('tar -zcfv $mypath web_static', shell=True)
+    local('mkdir -p versions/')
+    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = f"versions/web_static_{time_now}.tgz"
+    local(f'tar -cavf {file_path} web_static')
