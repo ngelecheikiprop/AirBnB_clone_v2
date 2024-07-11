bfcea7f55e811f6be42d591f0ef303e77f45d278
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-11 11:44:06 +0300

    number 2, corrected styling errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,6 +16,7 @@ def do_pack():
     local(f'tar -cavf {file_path} web_static')
     return file_path
 
+
 def do_deploy(archive_path):
     'distributes archive to webservers'
     if not os.path.exists(archive_path):
@@ -23,7 +24,8 @@ def do_deploy(archive_path):
     file_path = do_pack()
     run('mkdir -p /tmp/')
     put(file_path, '/tmp/')
-    run(f'tar -xvf /tmp/{file_path} --one-top-level -C /data/web_static/releases/')
+    run(f'tar -xvf /tmp/{file_path}'
+        '--one-top-level -C /data/web_static/releases/')
     run(f'rm /tmp/{file_path}')
     run(f'rm /data/web_static/current')
     run(f'ln -s {file_path} /data/web_static/current')
