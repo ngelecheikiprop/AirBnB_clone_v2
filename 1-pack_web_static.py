1c757e71f436f6c42294413e5c827b352cd833be
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-11 08:45:52 +0300

    number 1, added shell=True

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 'archieve web_static folder to deploy'
 from fabric.api import local
-def do_pack():
-    local('time_now=$(date "+%Y%m%d%H%M%S")')
-    local('tar -zcf web_static_<year><month><day><hour><minute><second>.tgz web_static')
 
+def do_pack():
+    local('mkdir -p versions', shell=True)
+    local('time_now=$(date "+%Y%m%d%H%M%S")', shell=True)
+    local('file_name=web_static_$time_now.tgz', shell=True)
+    local('mypath=versions/$file_name', shell=True)
+    local('tar -zcfv $mypath web_static', shell=True)
