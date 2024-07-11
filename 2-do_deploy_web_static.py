94507849f1a33141596ddb37651132bbc1de8506
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-11 11:47:05 +0300

    number 2, used the path variable gicen

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,12 +21,11 @@ def do_deploy(archive_path):
     'distributes archive to webservers'
     if not os.path.exists(archive_path):
         return False
-    file_path = do_pack()
     run('mkdir -p /tmp/')
-    put(file_path, '/tmp/')
-    run(f'tar -xvf /tmp/{file_path}'
+    put(archive_path, '/tmp/')
+    run(f'tar -xvf /tmp/{archive_path}'
         '--one-top-level -C /data/web_static/releases/')
-    run(f'rm /tmp/{file_path}')
+    run(f'rm /tmp/{archive_path}')
     run(f'rm /data/web_static/current')
-    run(f'ln -s {file_path} /data/web_static/current')
+    run(f'ln -s {archive_path} /data/web_static/current')
     return True
