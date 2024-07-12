7f7d3117835ac19351f4e28da4534fb388c51f62
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-12 04:42:21 +0300

    number 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
-#sets up webservers for deployment of webstatic
-
-sudo apt-get -y update
-sudo apt-get -y install nginx
-#make the neccesary directories
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-#make a fake index html
-sudo echo 'Hey there am now learning to deploy' | sudo tee /data/web_static/releases/test/index.html
-sudo rm -f /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/;
-SERVER_CONFIG="\
-server {
-	listen 80;
-	listen [::]:80 default_server;
-	server_name vitalisalexia.tech;
-	location /hbnb_static {
-		alias /data/web_static/current/;
-	}
-}\
-"
-sudo echo -e "$SERVER_CONFIG" | sudo tee /etc/nginx/sites-enabled/default
-sudo nginx -s reload
+# Setup a web servers for the deployment of web_static.
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
