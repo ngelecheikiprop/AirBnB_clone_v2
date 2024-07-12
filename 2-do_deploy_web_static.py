ad0ae2099797477bf1e2fb26eb22cc8870e2dc20
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-12 04:53:32 +0300

    number 2 and 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,37 +1,52 @@
 #!/usr/bin/python3
-'archieve web_static folder to deploy'
-from fabric.api import local, run, env, put, sudo
+"""
+Fabric script that distributes an archive to your web servers
+"""
+
 from datetime import datetime
+from fabric.api import *
 import os
-env.hosts = ['54.237.18.27', '34.232.66.181']
-env.user = 'ubuntu'
+
+env.hosts = ["54.237.18.27", "34.232.66.181"]
+env.user = "ubuntu"
 
 
 def do_pack():
-    'compress the webstatic folder into versions folder'
-    local('mkdir -p versions/')
-    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = f"versions/web_static_{time_now}.tgz"
-    local(f'cd web_static && tar -czvf ../{file_path} * && cd -')
-    return file_path
+    """
+        return the archive path if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    'distributes archive to webservers'
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        sudo('mkdir -p /tmp/')
-        put(archive_path, '/tmp/')
-        file_name = os.path.basename(archive_path)
-        sudo('mkdir -p /data/web_static/releases/')
-        sudo(f'tar -xf /tmp/{file_name} \
-                --one-top-level -C /data/web_static/releases/')
-        sudo(f'rm -f /tmp/{file_name}')
-        sudo(f'rm -f /data/web_static/current')
-        extracted_file = os.path.splitext(file_name)[0]
-        sudo(f'ln -s /data/web_static/releases/{extracted_file}\
-                /data/web_static/current')
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
         return True
-    except Exception:
-        return False
+
+    return False
