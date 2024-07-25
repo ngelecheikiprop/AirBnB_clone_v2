dc5fc5c9a8f197632bcb9e1a0cab10f5975c2ae7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-25 18:16:04 +0300

    number 4, print number if given only, added route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'flask application that returns text on the home route'
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    ' the home of the server'
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    ' displays hbnb '
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c_text(text):
+    'returns C and appends the text passed'
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text='is cool'):
+    'display python then text given'
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def print_number(n):
+    'display n only if number'
+    return f"{n}  is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
