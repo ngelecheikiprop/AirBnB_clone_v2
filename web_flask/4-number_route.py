4cd72eb85e0af01f847ec31d079d7250409c4f26
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-29 00:29:09 +0300

    added for number 3

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -23,6 +23,7 @@ def display_c_text(text):
     return f"C {text}"
 
 
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def display_python(text='is cool'):
     'display python then text given'
