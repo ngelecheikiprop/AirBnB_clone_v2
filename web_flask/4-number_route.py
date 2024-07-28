52ee8251562ccd64f84684b859fd0f7d7962c243
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-29 00:32:45 +0300

    removed an extra space

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def display_python(text='is cool'):
 @app.route("/number/<int:n>", strict_slashes=False)
 def print_number(n):
     'display n only if number'
-    return f"{n}  is a number"
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
